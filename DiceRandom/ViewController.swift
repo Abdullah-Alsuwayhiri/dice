15e4f952fcbe9b64eb71fca880b9dc93f757bd8b
AbdullahAlsuwayhiri <ab61di@gmail.com>
2020-04-16 11:50:58 -0700

    Initial Commit

---
+++ DiceRandom/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DiceRandom
+//
+//  Created by Abdullah Alsuwayhiri on 23/08/1441 AH.
+//  Copyright © 1441 Abdullah Alsuwayhiri. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
